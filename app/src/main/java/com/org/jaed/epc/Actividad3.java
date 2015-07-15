e6d64cf533c46fadb3d540255d0425283422f5fa
jaedjerry <jaedjerry26@gmail.com>
2015-07-15 15:41:19 -0600

    Rejected commit: Default

--- app/src/main/java/com/org/jaed/epc/Actividad3.java
+++ app/src/main/java/com/org/jaed/epc/Actividad3.java
@@ -19,7 +19,7 @@ import android.widget.TextView;
  * Created by Jerry on 13/07/2015.
  */
 public class Actividad3 extends AppCompatActivity {
-    private TextView txt1, txt2;
+    private TextView txt1, txt2, txt3;
     private Context context;
     private RelativeLayout marco;
     private Toolbar toolbar;
@@ -31,13 +31,14 @@ public class Actividad3 extends AppCompatActivity {
         setContentView(R.layout.actividad_3);
         txt1 = (TextView)findViewById(R.id.personajeAntagonico);
         txt2 = (TextView)findViewById(R.id.pacoMalo);
+        txt3 = (TextView)findViewById(R.id.perro);
         marco = (RelativeLayout)findViewById(R.id.marco);
         toolbar = (Toolbar)findViewById(R.id.appbar3);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         toolbar.setNavigationIcon(R.mipmap.ic_atras);
-        getSupportActionBar().setTitle("Arrastra de A a B");
+        getSupportActionBar().setTitle("Arrastra");
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,6 +50,7 @@ public class Actividad3 extends AppCompatActivity {
         });
         final Drawable original = txt2.getBackground();
         final Drawable original1 = txt1.getBackground();
+        final Drawable original2 = txt3.getBackground();
         final float txt2Xo = txt2.getX(), txt2Yo = txt2.getY();
         context = this;
         txt1.setOnTouchListener(new View.OnTouchListener() {
@@ -67,6 +69,7 @@ public class Actividad3 extends AppCompatActivity {
                         break;
                     case MotionEvent.ACTION_MOVE:
                         txt2.setVisibility(View.VISIBLE);
+                        txt3.setVisibility(View.VISIBLE);
                         RelativeLayout.LayoutParams layoutParams =
                                 (RelativeLayout.LayoutParams) v.getLayoutParams();
                         layoutParams.leftMargin = x - xDelta;
@@ -76,19 +79,32 @@ public class Actividad3 extends AppCompatActivity {
                         v.setLayoutParams(layoutParams);
                         if(x > (int)txt2.getX() - 70 && y > (int) txt2.getY() && y < txt2.getY() + 110){
                             txt2.setBackgroundResource(R.drawable.borde);
-                            Log.e("move", "posicion en x del target: "+String.valueOf(txt2.getX())+", "+"posicion en x del arrastre"+String.valueOf(x));
+                            //Log.e("move", "posicion en x del target: "+String.valueOf(txt2.getX())+", "+"posicion en x del arrastre"+String.valueOf(x));
                         }else{
                             txt2.setBackgroundDrawable(original);
-                            Log.e("falle en move","pposición en x del target"+String.valueOf(txt2.getX())+", "+"posicion en x del arrastre" + String.valueOf(x));
+                            //Log.e("falle en move","posicion en x del target"+String.valueOf(txt2.getX())+", "+"posicion en x del arrastre" + String.valueOf(x));
+                        }
+                        if(x>(int)txt3.getX() - 70 && y > (int)txt3.getY() && y < txt3.getY() + 110){
+                            txt3.setBackgroundResource(R.drawable.borde);
+                            Log.e("move", "condicion 2 posicion en x del target: " + String.valueOf(txt3.getX()) + ", " + "posicion en x del arrastre" + String.valueOf(x));
+                        }else{
+                            txt3.setBackgroundDrawable(original2);
+                            Log.e("falle en move","condicion 2 posicion en x del target"+String.valueOf(txt3.getX())+", "+"posicion en x del arrastre" + String.valueOf(x));
                         }
                         break;
                     case MotionEvent.ACTION_UP:
                         txt1.setBackgroundDrawable(original1);
                         if(x > (int)txt2.getX() - 70 && y > (int) txt2.getY() && y < txt2.getY() + 110){
-                            Log.e("entre", "entre en action up");
+                            //Log.e("entre", "entre en action up");
                             txt2.setVisibility(View.GONE);
                             txt1.setX(txt2.getX()-50);
                             txt1.setY(txt2.getY());
+                        }
+                        if(x>(int)txt3.getX() - 70 && y > (int)txt3.getY() && y < txt3.getY() + 110) {
+                            Log.e("up", "entre en up de la condición 2");
+                            txt3.setVisibility(View.GONE);
+                            txt1.setX(txt3.getX()-60);
+                            txt1.setY(txt3.getY());
                         }
                         break;
 
